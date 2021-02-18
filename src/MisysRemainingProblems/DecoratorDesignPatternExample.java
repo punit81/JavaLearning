579e4b6297e070d488cb8e52c0ac7dc40b9f5895
punit81 <punitkumartiwari81@gmail.com>
2021-02-18 21:42:19 +0530

    [Explain a situation where decorator design pattern can be used with the help of a program.]

---
+++ src/MisysRemainingProblems/DecoratorDesignPatternExample.java
@@ -0,0 +1,78 @@
+package MisysRemainingProblems;
+interface Food {
+	String getName();
+	double getPrice();
+}
+class VegFood implements Food{
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Veg Food";
+	}
+
+	@Override
+	public double getPrice() {
+		// TODO Auto-generated method stub
+		return 60.0;
+	}
+	
+}
+abstract class NonVegFoodDecorator implements Food{
+	private Food food;
+	public NonVegFoodDecorator(Food food) {
+		this.food=food;
+	}
+	public String getName() {
+		return food.getName();
+	}
+	public double getPrice() {
+		return food.getPrice();
+	}
+}
+class ChineseFood extends NonVegFoodDecorator{
+
+	public ChineseFood(Food food) {
+		super(food);
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return super.getName()+"Chinese Food";
+	}
+	@Override
+	public double getPrice() {
+		// TODO Auto-generated method stub
+		return super.getPrice()+40.0;
+	}
+	
+}
+class IndianNonVegFood extends NonVegFoodDecorator{
+
+	public IndianNonVegFood(Food food) {
+		super(food);
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return super.getName()+"Indian Non-Veg Food";
+	}
+	@Override
+	public double getPrice() {
+		// TODO Auto-generated method stub
+		return super.getPrice()+50.0;
+	}
+	
+}
+public class DecoratorDesignPatternExample {
+
+	public static void main(String[] args) {
+		Food food=new VegFood();
+		System.out.println("Food Name:"+food.getName()+"\tPrice:"+food.getPrice());
+		Food food1=new IndianNonVegFood(new VegFood());
+		System.out.println("Food Name:"+food1.getName()+"\tPrice:"+food1.getPrice());
+		Food food2=new ChineseFood(new VegFood());
+		System.out.println("Food Name:"+food2.getName()+"\tPrice:"+food2.getPrice());
+	}
+
+}
