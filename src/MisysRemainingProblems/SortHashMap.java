8722e511bb0854003b5e8a14b7897c505fa8deb3
punit81 <punitkumartiwari81@gmail.com>
2021-02-18 23:40:43 +0530

    [Added second method in SortHashMap i.e without using TreeMap Approach]

--- src/MisysRemainingProblems/SortHashMap.java
+++ src/MisysRemainingProblems/SortHashMap.java
@@ -1,7 +1,12 @@
 package MisysRemainingProblems;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 public class SortHashMap {
@@ -17,6 +22,18 @@ public class SortHashMap {
 		SortedMap<String,Integer> tmap=new TreeMap<String,Integer>();
 		tmap.putAll(hmap);
 		System.out.println(tmap);
+		//Second method without using TreeMap
+		Map<String,Integer>lmap=new LinkedHashMap<String,Integer>();
+		Set<String> keys=hmap.keySet();
+		List<String> alist=new ArrayList();
+		alist.addAll(keys);
+		alist.sort(new Comparator<String>() {
+			public int compare(String str1,String str2) {
+				return str1.compareTo(str2);
+			}
+		});
+		alist.forEach(al->lmap.put(al, hmap.get(al)));
+		System.out.println(lmap);
 	}
 
 }
